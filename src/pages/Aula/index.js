3842192da4c34d75c420e9c943ca6e003ab250f8
Kipperfeito <heitor.k.w@gmail.com>
2025-09-08 12:24:46 -0300

    cabecalho
    falta imagem para tema escuro

--- src/pages/Aula/index.js
+++ src/pages/Aula/index.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 export default function Aula() {
-return (
-<>
-<h1>Indie Hub</ h1>
-<div></div>
-<p>Senta no bugalu</p>
-</>
-);
+  return (
+    <>
+      <h1>Indie Hub</h1>
+      <div></div>
+      <p>Senta no bugalu</p>
+    </>
+  );
 }
